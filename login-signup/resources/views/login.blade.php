f6fdb2a24cfe29515edf3bca57a74ecbb21273e8
ran <rdavee0412@gmail.com>
2024-01-10 13:25:57 +0800

    final layouts

--- login-signup/resources/views/login.blade.php
+++ login-signup/resources/views/login.blade.php
@@ -1,52 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <!--========== START OF LOGIN SECTION ==========-->
     <div class="wrapper">
         <div class="position-absolute mt-1 container-fluid">
             @if($errors->any())
-                <div class="col-12">
-                    @foreach ($errors -> all() as $error)
-                        <div class="alert alert-danger">{{$error}}</div>
-                    @endforeach
-                </div>
+            <div class="col-12">
+                @foreach ($errors -> all() as $error)
+                <div class="alert alert-danger">{{$error}}</div>
+                @endforeach
+            </div>
             @endif
 
             @if(session() -> has('error'))
-                <div class="alert alert-danger">{{session('error')}}</div>
+            <div class="alert alert-danger">{{session('error')}}</div>
             @endif
 
             @if(session() -> has('success'))
-                <div class="alert alert-success">{{session('success')}}</div>
+            <div class="alert alert-success">{{session('success')}}</div>
             @endif
         </div>
         <div class="container main py-5">
             <div class="row">
-                <div class="col-md-6 side-image">      
-                    <!-- <div class="text">
-                        <p>Join the community of D=MAC²</p>
-                    </div> -->
+                <div class="col-md-6 side-image">
+                    <div class="text">
+                        <p>MAKING LIVES BETTER THROUGH EDUCATION</p>
+                    </div>
                 </div>
                 <div class="col-md-6 right">
                     <form action="{{route('login_post')}}" method="POST" class="input-box">
-                        @csrf 
-                        <header>Welcome to D=MAC²</header>                     
+                        @csrf
+                        <header>PHINMA<span class="katanungan">Katanungan</span></header>
                         <div class="input-field">
                             <input type="text" class="input" id="email" name="email" required autocomplete="off">
                             <label for="email">Email</label>
                         </div>
                         <div class="input-field">
-                            <input type="password" class="input"  id="password" name="password" required>
+                            <input type="password" class="input" id="password" name="password" required>
                             <label for="password">Password</label>
                         </div>
-                        <div>
+                        <div class="forget">
                             <a href="{{route('forget_password')}}">Forget Password</a>
                         </div>
                         <div class="input-field">
@@ -64,4 +66,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
     <script src="/src/components/main.js"></script>
 </body>
+
 </html>
--- login-signup/resources/views/signup.blade.php
+++ login-signup/resources/views/signup.blade.php
@@ -1,43 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup Page</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <!--========== START OF SIGNUP SECTION ==========-->
     <div class="wrapper">
         <div class="position-absolute mt-1 container-fluid">
             @if($errors->any())
-                <div class="col-12">
-                    @foreach ($errors -> all() as $error)
-                        <div class="alert alert-danger">{{$error}}</div>
-                    @endforeach
-                </div>
+            <div class="col-12">
+                @foreach ($errors -> all() as $error)
+                <div class="alert alert-danger">{{$error}}</div>
+                @endforeach
+            </div>
             @endif
 
             @if(session() -> has('error'))
-                <div class="alert alert-danger">{{session('error')}}</div>
+            <div class="alert alert-danger">{{session('error')}}</div>
             @endif
 
             @if(session() -> has('success'))
-                <div class="alert alert-success">{{session('success')}}</div>
+            <div class="alert alert-success">{{session('success')}}</div>
             @endif
         </div>
         <div class="container main py-5">
             <div class="row">
-                <div class="col-md-6 side-image">      
-                    <!-- <div class="text">
-                        <p>Join the community of D=MAC²</p>
-                    </div> -->
+                <div class="col-md-6 side-image">
+                    <div class="text">
+                        <p>Join the community</p>
+                    </div>
                 </div>
                 <div class="col-md-6 right">
                     <form action="{{route('signup_post')}}" method="POST" class="input-box">
-                        @csrf 
-                        <header>Welcome to D=MAC²</header>
+                        @csrf
+                        <header>Welcome to PHINMA<span class="katanungan">Katanungan</span></header>
                         <div class="input-field">
                             <input type="text" class="input" id="name" name="name" required autocomplete="off">
                             <label for="name">Name</label>
@@ -69,4 +71,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
     <script src="/src/components/main.js"></script>
 </body>
+
 </html>
